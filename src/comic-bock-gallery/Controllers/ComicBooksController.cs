f427b527abd472d5dc6feee8c14f2ec3978fdcf5
Irokanji <Irokanji@DESKTOP-4HMTC6H>
2018-07-02 22:26:27 +0000

    Added comic books action methode

--- src/comic-bock-gallery/Controllers/ComicBooksController.cs
+++ src/comic-bock-gallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace comic_bock_gallery.Controllers
 {
     public class ComicBooksController:Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller !!";
+        }
     }
 }
